6953e5c08e1090804836a760f0e79398188b31d8
Stein Vandenbroeke <steinvandenbroeke@gmail.com>
2025-05-17 11:55:28 +0200

    data loader

--- data_processing.py
+++ data_processing.py
@@ -10,13 +10,23 @@ from collections import defaultdict
 
 
 class Messenger_data(Dataset):
-    def __init__(self, messages_folder="messages"):
+    def __init__(self, messages_folder="messages", save_path="data/messages.pt"):
+        self.messages_data = {}
         print(self.__len__())
+        if os.path.exists(save_path):
+            self.messages_data = torch.load(save_path)
+        else:
+            self.messages_data = self.create_dataset(messages_folder)
+            torch.save(self.messages_data, save_path)
+
+
+    def create_dataset(self, messages_folder="messages"):
+
 
     # TODO
     def __getitem__(self, index):
-        return self.data[index], self.labels[index][1]
+        return self.messages_data[index]["message"], self.messages_data[index]["person"], self.messages_data[index]["time"], self.messages_data[index]["group"]
 
     # TODO
     def __len__(self):
-        return self.n_samples
+        return self.messages_data.__len__()
